48cddd8b517c3a06d3b59a2ad2f64bef585efc02
Lizzyfemme <lizzy_swann@hotmail.com>
2019-10-08 21:20:44 +0000

    counter counting

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,9 @@
 
-$(document).keypress(()=> {
-  console.log("Hello")
+
+$(document).ready(() => {
+  var textCounter = $(".counter")
+  $("textarea").keypress((event)=>{
+    console.log(event.target.textLength)
+    textCounter.text(event.target.textLength)
+  });
 });
