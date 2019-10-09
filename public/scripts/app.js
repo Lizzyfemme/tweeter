b3885095939e9bbc98f7ededd377ce376ad005b6
Lizzyfemme <lizzy_swann@hotmail.com>
2019-10-09 20:46:06 +0000

    tweets added dynamically

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,62 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// Fake data taken from initial-tweets.json
+const dataTEst = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+
+const renderTweets = function(tweets) {
+for (let tweet of tweets){
+  console.log("tweet of renderTweets", tweet)
+  const $tweetElement = createTweetElement(tweet)
+  console.log("renderTweets after the cb ",$tweetElement)
+  //main.appendChild(tweetElement)
+  $('main').append($tweetElement)
+}
+
+// takes return value and appends it to the tweets container
+}
+
+const createTweetElement = function(tweet) {
+
+  const markup = `
+<header>
+  <h1>${tweet.user.name}</h1>
+  <img src =${tweet.user.avatars}>
+  <h3>${tweet.user.handle}</h3>
+</header>
+<p>${tweet.content.text}</p>
+<footer>
+  <h2>${tweet.content.create_at}</h2>
+  <img src="/images/heart.png">
+  <img src="/images/flag.png">
+  <img src="/images/square.jpg">
+</footer>
+`
+let $tweetElement = $('<article></article>').addClass('postedTweet');
+console.log($tweetElement)
+$tweetElement.append(markup);
+
+return $tweetElement;
+};
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,30 @@
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+
+
 $(document).ready(() => {
   let textCounter = $(".counter");
   let textArea= $("textarea");
@@ -11,8 +38,5 @@ $(document).ready(() => {
       textCounter.removeClass ('negCounter');
     }
   });
+  renderTweets(data)
 });
-    // if ((140 - (event.target.textLength+1)) <= 0 ) {
-    //   (".counter").removeClass(".counter").addClass(".negCounter")
-    // }
-  //}
