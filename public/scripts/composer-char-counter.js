0eb508b4b235d2b46e89c5e120780a52a6440002
Lizzyfemme <lizzy_swann@hotmail.com>
2019-10-08 17:17:05 -0700

    Counter Is Prefect!

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,18 @@
-
-
 $(document).ready(() => {
-  let textCounter = $(".counter")
-  textCounter.text(140);
-  $("textarea").keypress((event)=>{
-    textCounter.text(140 - (event.target.textLength + 1))
+  let textCounter = $(".counter");
+  let textArea= $("textarea");
+  textCounter.html(140);
+  textArea.on('keyup', function () {
+    let charCount = 140 - $(this).val().length;
+    textCounter.html(charCount);
+    if (charCount < 0) {
+      textCounter.addClass ('negCounter');
+    } else {
+      textCounter.removeClass ('negCounter');
+    }
   });
 });
-
-let subtract = function(){
-
-};
+    // if ((140 - (event.target.textLength+1)) <= 0 ) {
+    //   (".counter").removeClass(".counter").addClass(".negCounter")
+    // }
+  //}
