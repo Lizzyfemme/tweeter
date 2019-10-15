aeeaaa1b97fff57483f091a6dbfb235023f1a9c6
Lizzyfemme <lizzy_swann@hotmail.com>
2019-10-15 16:54:05 -0700

    lint

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,30 +1,29 @@
 
-// validateTweet is a function that checks the tweets for errors, it returns true if the tweet is valid
-const validateTweet = function (data) {
-  if (!data) throw "Tweets must contain data. Please edit your tweet."
-  if (data.length > 140) throw "Sorry there is a 140 character maximum for tweets. Please shorten your message"
+// validateTweet is a function that checks the tweets for errors
+const validateTweet = function(data) {
+  if (!data) throw "Tweets must contain data. Please edit your tweet.";
+  if (data.length > 140) throw "Sorry there is a 140 character maximum for tweets. Please shorten your message";
   return true;
 };
-
-const throwError = function (msg) {
-   if (msg) {
+//if tweets do not pass validateTweet an error will be thrown
+const throwError = function(msg) {
+  if (msg) {
     const errorArea = $('.errorHandling');
-    // "Tweets must contain data. Please edit your tweet."
     errorArea.html(msg).show();
-   }
- };
+  }
+};
 
 /* renderTweets is a function that interates over the tweets and adds then to the tweet container.*/
 
-const renderTweets = function (tweets) {
+const renderTweets = function(tweets) {
   for (let tweet of tweets) {
-    const $tweetElement = createTweetElement(tweet)
-    $('#tweet-container').prepend($tweetElement)
+    const $tweetElement = createTweetElement(tweet);
+    $('#tweet-container').prepend($tweetElement);
   }
-}
+};
 
 //createTweetElement is a function that formats the tweets to the correct html
-const createTweetElement = function (tweet) {
+const createTweetElement = function(tweet) {
   const markup = `
 <header>
   <div class= "nameAndPic">
@@ -42,45 +41,45 @@ const createTweetElement = function (tweet) {
     <img src="/images/square.jpg">
   </div>
 </footer>
-`
+`;
   let $tweetElement = $('<article></article>').addClass('postedTweet');
   $tweetElement.html(markup);
   return $tweetElement;
 };
 //loadTweets is a function that retrieve tweets
-const loadTweets = function () {
+const loadTweets = function() {
   $.ajax({
     url: "/tweets",
     method: "GET",
-    success: function (data) {
+    success: function(data) {
       $('#tweet-container').html('');
-      renderTweets(data)
+      renderTweets(data);
     }
-  })
-}
+  });
+};
 //where new tweets are validated, posted and displayed
 $(document).ready(() => {
   loadTweets();
-  $(".tweet-form").on("submit", function (event) {
+  $(".tweet-form").on("submit", function(event) {
     event.preventDefault();
-  const errorArea = $('.errorHandling');
-  errorArea.hide();
-  try {
-    validateTweet($(".new-tweet-input").val());
-    $.ajax({
-      url: "/tweets",
-      method: "POST",
-      data: $(".tweet-form").serialize(),
-      success: function (response) {
-        loadTweets();
-        //new line
-        $(".new-tweet-input").val('');
-      }
-    })
-  } catch (msg) {
-    throwError (msg);
-  }
-});
+    const errorArea = $('.errorHandling');
+    errorArea.hide();
+    try {
+      validateTweet($(".new-tweet-input").val());
+      $.ajax({
+        url: "/tweets",
+        method: "POST",
+        data: $(".tweet-form").serialize(),
+        success: function(response) {
+          loadTweets();
+          //new line
+          $(".new-tweet-input").val('');
+        }
+      });
+    } catch (msg) {
+      throwError(msg);
+    }
+  });
   
 });
 
