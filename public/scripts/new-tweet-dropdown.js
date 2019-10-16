9410cc25e750d83238dac99bda1de2546da09f4c
Lizzyfemme <lizzy_swann@hotmail.com>
2019-10-15 17:51:57 -0700

    textarea ready to accept input

--- public/scripts/new-tweet-dropdown.js
+++ public/scripts/new-tweet-dropdown.js
@@ -1,5 +1,7 @@
 //This makes the tweet field slide up or down when clicked
 $("button").click(function () {
   $(".new-tweet").slideToggle("slow", function () {
+    $(".new-tweet-input").focus();
   });
+
 });
